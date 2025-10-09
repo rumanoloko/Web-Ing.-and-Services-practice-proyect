49dbaa7af87d764d9b395975532db91066feaea9
rumanoLoko <pasatpetruvlad@gmail.com>
2025-10-09 23:44:43 +0200

    Modificacion del fichero semilla generador de la base de datos

--- db/seeder.ts
+++ db/seeder.ts
@@ -3,7 +3,8 @@ import Products, { Product } from '@/models/Product';
 import Orders, { Order, OrderItem} from '@/models/Order';
 import dotenv from'dotenv';
 
- import mongoose from'mongoose';
+ import mongoose, { Types } from 'mongoose'
+import bcrypt from 'bcrypt'
  dotenv.config({ path: `.env.local`, override:true});
  const MONGODB_URI = process.env.MONGODB_URI;
  async function seed() {
@@ -18,56 +19,162 @@ import dotenv from'dotenv';
  }
 
 const products: Product[] = [
-  {
-  name: 'Earthen Bottle',
-  price: 39.95,
-  img: '/img/ecommerce-images/image-card-01.jpg',
-  description: 'What a bottle!',
-  },
-  {
-  name: 'Nomad Tumbler',
-  price: 39.95,
-  img: '/img/ecommerce-images/image-card-02.jpg',
-  description: 'Yet another item',
-  },
  {
   name: 'M5 CS F90',
   price: 175000,
-  img: 'public/img/m5',
+  img: '/img/m5.png',
   description: 'Best german sedan and maybe the last not hybrid 5 series model',
  },
  {
   name: 'CLS 63s W218',
   price: 55000,
-  img: 'public/img/cls63s',
+  img: '/img/cls63s.png',
   description: 'Most iconic Mercedes sport coupe luxury car',
  },
  {
   name: 'M6 Grand Coupe f06',
   price: 75000,
-  img: 'public/img/m6',
+  img: '/img/m6.png',
   description: 'Last 6 Series model before 8 series beginning',
  },
  {
   name: 'Q7 V12 TDI',
   price: 80000,
-  img: 'public/img/q7',
+  img: '/img/q7.png',
   description: 'Most unusual V12 engine with diesel fuel ',
  },
  {
-  name: 'Phaeton V12',
+  name: 'VW Phaeton V12',
   price: 67000,
-  img: 'public/img/phaeton',
+  img: '/img/phaeton.png',
   description: 'At the same time the best and worse VW model',
+ },
+ {
+  name: 'VW Arteon',
+  price: 70000,
+  img: '/img/arteon.png',
+  description: 'No description',
+ },
+ {
+  name: 'Toyota Century V12',
+  price: 35000,
+  img: '/img/century_v12.png',
+  description: 'No description',
+ },
+ {
+  name: 'Chrysler 300',
+  price: 50000,
+  img: '/img/chrysler300.png',
+  description: 'No description',
+ },
+ {
+  name: 'Alfa Romeo Giulia',
+  price: 80000,
+  img: '/img/giulia.png',
+  description: 'No description',
+ },
+ {
+  name: 'Bentley GT Continental',
+  price: 110000,
+  img: '/img/gt_continental.png',
+  description: 'No description',
+ }
+ ,{
+  name: 'Toyota Land Cruiser 300',
+  price: 90000,
+  img: '/img/landcruiser.png',
+  description: 'No description',
+ },
+ {
+  name: 'Mercedes W140',
+  price: 23000,
+  img: '/img/mercedes_w140.png',
+  description: 'No description',
+ },
+ {
+  name: 'Audi R8',
+  price: 180000,
+  img: '/img/r8.png',
+  description: 'No description',
+ },
+ {
+  name: 'Range Rover STR',
+  price: 145000,
+  img: '/img/range_rover_sport.png',
+  description: 'No description',
+ },
+ {
+  name: 'Ford Raptor',
+  price: 118000,
+  img: '/img/raptor.png',
+  description: 'No description',
+ },
+ {
+  name: 'Audi RS6',
+  price: 120000,
+  img: '/img/rs6.png',
+  description: 'No description',
+ },
+ {
+  name: 'Audi RS7',
+  price: 127000,
+  img: '/img/rs7.png',
+  description: 'No description',
+ },
+ {
+  name: 'Audi RSQ8',
+  price: 135000,
+  img: '/img/rsq8.png',
+  description: 'No description',
+ },
+ {
+  name: 'Audi S8',
+  price: 155000,
+  img: '/img/s8.png',
+  description: 'No description',
+ },
+ {
+  name: 'Alfa Romeo Stelvio',
+  price: 76000,
+  img: '/img/stelvio.png',
+  description: 'No description',
+ },
+ {
+  name: 'Jeep TrackHawk',
+  price: 112000,
+  img: '/img/trackhawk.png',
+  description: 'No description',
+ },
+ {
+  name: 'VW Tuareg',
+  price: 89000,
+  img: '/img/tuareg.png',
+  description: 'No description',
+ },
+ {
+  name: 'Land Rover Velar',
+  price: 78000,
+  img: '/img/velar.png',
+  description: 'No description',
+ },
+ {
+  name: 'BWW X6',
+  price: 158000,
+  img: '/img/x6.png',
+  description: 'No description',
+ },
+ {
+  name: 'Ultimate Torreto\'s Shopping Cart RT Deluxe Ultima Edition',
+  price: 158000,
+  img: '/img/x6.png',
+  description: 'No description',
  }
  ];
 
- //await conn.connection.db.dropDatabase();
+ await conn.connection.db.dropDatabase();
  // Do things here.
- //const insertedProducts= await Products.create(products);
-  const insertedProducts= await Products.find();
- console.log(JSON.stringify(insertedProducts, null, 2));
- console.log('Producto id =>', insertedProducts[0]._id)
+ const insertedProducts= await Products.create(products);
+ //const insertedProducts= await Products.find();
 
 
  const orders: Order[] = [
@@ -140,10 +247,8 @@ const products: Product[] = [
   }
  ]
 
- //const insertedOrders = await Orders.create(orders);
-  const insertedOrders = await Orders.find();
- console.log('Orders created in local_mongoDB');
- console.log(JSON.stringify(insertedOrders, null, 2));
+ const insertedOrders = await Orders.create(orders);
+  //const insertedOrders = await Orders.find();
  const user: User= {
   email: 'johndoe@example.com',
   password:'1234',
@@ -165,11 +270,10 @@ const products: Product[] = [
     insertedOrders[0]._id, insertedOrders[1]._id
   ],
   };
-
- //const res= await Users.create(user);
-  const res= await Users.find();
- console.log('User created in local_mongoDB');
- console.log(JSON.stringify(res, null, 2));
+ const hash = await bcrypt.hash(user.password, 10)
+  user.password = hash
+ const res= await Users.create(user);
+  //const res= await Users.find();
 
 
   const userProjection = {
@@ -190,10 +294,6 @@ const products: Product[] = [
      .populate({path: 'orders', select: '-__v'})
      .select('-_id orders')
      .lean();
- console.log("ORDERS")
-  const resu = await Orders.find();
- console.log(JSON.stringify(resu, null, 2));
- console.log(mongoose.modelNames())
  await conn.disconnect();
  }
- seed().catch(console.error);
+seed().catch(console.error);
