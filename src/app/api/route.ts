6af351261a2572000293720da80792f039611a0b
rumanoLoko <pasatpetruvlad@gmail.com>
2025-12-25 19:42:02 +0100

    Modificación de app/api/route.ts

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -1,4 +1,5 @@
-import { NextRequest } from 'next/server'
+import {NextRequest, NextResponse} from 'next/server'
 
 export async function GET(request: NextRequest) {
+    return NextResponse.json( {ok: true});
 }
